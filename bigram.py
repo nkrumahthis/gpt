249fc5ba2bd89d411fd7c15f79752c2d5e926d16
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-08-12 20:10:02 +0000

    applying version 4

--- bigram.py
+++ bigram.py
@@ -10,6 +10,7 @@ eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32 # number of embedding options
 #-------------------
 
 torch.manual_seed(1111)
@@ -56,15 +57,18 @@ def estimate_loss():
     model.train()
     return out
 
+# super simple bigram model
 class BigramLanguagemodel(nn.Module):
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
         # idx and targets are both (B,T) tensor of integers
-        logits = self.token_embedding_table(idx) # (B,T,C)
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        logits = self.lm_head(tok_emb) # (B,T,vocab_size)
 
         if targets is None:
             loss = None
@@ -91,7 +95,7 @@ class BigramLanguagemodel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
     
-model = BigramLanguagemodel(vocab_size)
+model = BigramLanguagemodel()
 m = model.to(device)
 
 # create a PyTorch optimizer
